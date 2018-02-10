c9cb54d425df52db0597cdac102a26b1bb7d082a
Nikan <32056409+Nikan888@users.noreply.github.com>
2018-02-10 18:21:27 +0300

    Task #6

---
+++ Task6/Employee.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    public class Employee : IComparable
+    {
+        string firstName;
+        string lastName;
+        int age;
+        string homeAdress;
+        string department;
+        string post;
+        string workPhoneNumber;
+        decimal salary;
+
+        public string FirstName { get => firstName; private set => firstName = value; }
+        public string LastName { get => lastName; private set => lastName = value; }
+        public string HomeAdress { get => homeAdress; private set => homeAdress = value; }
+        public string Department { get => department; private set => department = value; }
+        public string Post { get => post; private set => post = value; }
+        public string WorkPhoneNumber { get => workPhoneNumber; private set => workPhoneNumber = value; }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                if (value <= 14)
+                {
+                    throw new Exception("Age must be more than 13");
+                }
+                age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Salary must be more than 0");
+                }
+                salary = value;
+            }
+        }
+
+        public Employee(string firstName, string lastName, int age, string homeAdress,
+            string department, string post, string workPhoneNumber, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            HomeAdress = homeAdress;
+            Department = department;
+            Post = post;
+            WorkPhoneNumber = workPhoneNumber;
+            Salary = salary;
+        }
+
+        //method for comparing class execs by the value of a salary
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Employee))
+            {
+                throw new Exception("Object isn't Employee");
+            }
+            Employee employee = (Employee)obj;
+            if (this > employee)
+                return 1;
+            else if (this < employee)
+                return -1;
+            else
+                return 0;
+        }
+
+        public static bool operator >(Employee e1, Employee e2)
+        {
+            return (e1.Salary > e2.Salary);
+        }
+
+        public static bool operator <(Employee e1, Employee e2)
+        {
+            return (e2 > e1);
+        }
+
+        public override string ToString()
+        {
+            return (FirstName + ';' + LastName + ';' + Age + ';' + HomeAdress + ';' + Department
+                + ';' + Post + ';' + WorkPhoneNumber + ';' + Salary);
+        }
+    }
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>
+            {
+                new Employee("Yury", "Bolbukou", 18, "Oktyabrya, 48a", "BI", "trainee", "+375293595413", 25),
+                new Employee("Stanislav", "Ivanov", 20, "Gagarina, 20", "BI", "junior", "+375441158736", 154)
+            };
+            try
+            {
+                list.Add(new Employee("Stanislav", "Verevkin", 12, "Masurova, 25", "BI", "trainee", "+375298745214", 50));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Default list:");
+            foreach (var e in list)
+            {
+                Console.WriteLine(e);
+            }
+            list.Sort();
+            Console.WriteLine("Sorted list:");
+            foreach (var e in list)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadKey();
+        }
+    }
+}
